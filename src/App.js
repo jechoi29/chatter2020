3688ac46e253b8c57fbc2cf4124c36c5d2564d4b
Jungeun Choi <jechoi29@uw.edu>
2020-01-17 09:22:31 -0800

    Add chatter app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <main>
+
+    <header>
+      <img classname="logo"
+        alt="logo"
+        src="https://cdn2.iconfinder.com/data/icons/ios7-inspired-mac-icon-set/1024/messages_5122x.png"
+      />
+      Chatter
+  </header>
+
+    <div class="received-msg">
+      <div class="arrow-left"></div>
+      <div class="received-msg-inbox">
+        <p>Hi, Jung!!</p>
+      </div>
     </div>
-  );
+
+    <div class="sending-msg">
+      <div class="arrow-right"></div>
+      <div class="sending-msg-inbox">
+        <p>Hi, You!!❤️</p>
+      </div>
+    </div>
+
+    <TextInput onSend={m => console.log(m)} />
+
+  </main>
 }
+
+function TextInput(props) {
+  const [text, setText] = useState('')
+
+  return <div className="textbox">
+    <div className="text-input-wrap">
+      {/*       <input value={text} className="text-input"
+        placeholder="write your message here..."
+        onChange={e => setText(target.value)}
+      /> */}
+
+      <button onClick={() => {
+        props.onSend(text)
+        setText('')
+      }} className="button">
+        →
+    </button>
+    </div>
+  </div>
+}
+
 
 export default App;
