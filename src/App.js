01a97232c475f081dfec54e2ee31ec0cd4e45056
Jungeun Choi <jechoi29@uw.edu>
2020-01-22 10:52:19 -0800

    Set username and server

--- src/App.js
+++ src/App.js
@@ -1,66 +1,91 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import NamePicker from "./namePicker.js";
+import { db } from "./db";
 
 function App() {
-  {/* store messages into an empty array */ }
-  const [messages, setMessages] = useState([])
+  // store messages into an empty array
+  const [messages, setMessages] = useState([]);
+  const [name, setName] = useState("Jung");
+
+  useEffect(() => {
+    db.listen({
+      receive: m => setMessages(current => [m, ...current])
+    });
+  }, []);
 
   // check how messages are shown
-  console.log(messages)
+  console.log(messages);
 
-  return <main>
-
-    <header>
-      <img className="logo"
-        alt="logo"
-        src="https://cdn2.iconfinder.com/data/icons/ios7-inspired-mac-icon-set/1024/messages_5122x.png"
-      />
-      Chatter
-  </header>
-
-    {/* show text messages */}
-    <div className="messages">
-      {messages.map((m, i) => {
-        return <div key={i} className="message-wrap">
-          <div className="message">{m}</div>
+  return (
+    <main>
+      <header>
+        <div className="logo-wrap">
+          <img
+            className="logo"
+            alt="logo"
+            src="https://cdn2.iconfinder.com/data/icons/ios7-inspired-mac-icon-set/1024/messages_5122x.png"
+          />
+          Chatter
         </div>
-      })}
-    </div>
+        <NamePicker onSave={setName} />
+      </header>
 
-    <TextInput onSend={(text) => {
-      // new messages at the beginning of the array
-      setMessages([text, ...messages])
-    }} />
+      {/* show text messages */}
+      <div className="messages">
+        {messages.map((m, i) => {
+          return (
+            <div key={i} className="message-wrap">
+              <div className="message">{m.text}</div>
+            </div>
+          );
+        })}
+      </div>
 
-  </main>
+      <TextInput
+        onSend={text => {
+          db.send({
+            text,
+            name,
+            ts: new Date()
+          });
+        }}
+      />
+    </main>
+  );
 }
 
 function TextInput(props) {
-  var [text, setText] = useState('')
+  var [text, setText] = useState("");
 
   // js comment
-  return <div className="textbox">
-    <input value={text}
-      className="textbox-input"
-      placeholder="write your message here..."
-      onChange={e => setText(e.target.value)}
-      // in the textbox, pressed the enter key, then sent message
-      onKeyPress={e => {
-        if (e.key === 'Enter') {
-          if (text) props.onSend(text)
-          setText('')
-        }
-      }}
-    />
-    <button onClick={() => {
-      if (text) props.onSend(text)
-      setText('')
-    }} className="button"
-      disabled={!text}>
-      SEND
-    </button>
-  </div>
+  return (
+    <div className="textbox">
+      <input
+        value={text}
+        className="textbox-input"
+        placeholder="write your message here..."
+        onChange={e => setText(e.target.value)}
+        // in the textbox, pressed the enter key, then sent message
+        onKeyPress={e => {
+          if (e.key === "Enter") {
+            if (text) props.onSend(text);
+            setText("");
+          }
+        }}
+      />
+      <button
+        onClick={() => {
+          if (text) props.onSend(text);
+          setText("");
+        }}
+        className="button"
+        disabled={!text}
+      >
+        SEND
+      </button>
+    </div>
+  );
 }
-
 
 export default App;
